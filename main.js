1094b6a248812bef7de11743f581f4c8c9894ee8
Jazz <104627239+jazzpizazz@users.noreply.github.com>
2024-09-23 10:50:27 +0200

    Add support for using network interfaces instead of only IPs

--- main.js
+++ main.js
@@ -1,26 +1,52 @@
-const { PluginSettingTab, Plugin, Setting } = require('obsidian');
+const { PluginSettingTab, Plugin, Setting, Notice } = require('obsidian');
+const { networkInterfaces } = require('os');
 
 class IPReplacerPlugin extends Plugin {
-  ipAddress = '127.0.0.1';
-  toReplace = 'ATTACKER_IP';
+  replacement = 'lo';
+  placeholder = 'ATTACKER_IP';
+  networkInterfaces = {};
 
   async onload() {
     console.log('IP Replacer Loaded');
+    this.refreshInterfaces();
     this.addSettingTab(new IPReplacerSettingTab(this.app, this));
     this.registerMarkdownPostProcessor((element, context) => {
-      const replaceRegexp = new RegExp(this.toReplace, 'g');
+      const replaceRegexp = new RegExp(this.placeholder, 'g');
+      const resolvedIP = this.resolveReplacementIP();
       element.querySelectorAll('*').forEach(el => {
-        el.innerHTML = el.innerHTML.replace(replaceRegexp, this.ipAddress);
+        el.innerHTML = el.innerHTML.replace(replaceRegexp, resolvedIP);
       });
     });
   }
 
-  setIPAddress(newIP) {
-    this.ipAddress = newIP;
+  refreshInterfaces() {
+    this.networkInterfaces = networkInterfaces();
+    console.log(Object.keys(this.networkInterfaces));
+    new Notice('IP Replacer: Network interfaces refreshed');
   }
 
-  setToReplace(newToReplace) {
-    this.toReplace = newToReplace;
+  resolveReplacementIP() {
+    const networkInterface = this.networkInterfaces[this.replacement];
+    if (networkInterface) {
+      const ipV4Info = networkInterface.find(info => info.family === 'IPv4');
+      if (ipV4Info) {
+        return ipV4Info.address;
+      }
+      const ipV6Info = networkInterface.find(info => info.family === 'IPv6');
+      if (ipV6Info) {
+        return ipV6Info.address;
+      }
+
+    }
+    return this.replacement;
+  }
+
+  setReplacement(newReplacement) {
+    this.replacement = newReplacement;
+  }
+
+  setPlaceholder(newPlaceholder) {
+    this.placeholder = newPlaceholder;
   }
 
   onunload() {
@@ -41,24 +67,35 @@ class IPReplacerSettingTab extends PluginSettingTab {
     containerEl.createEl('h2', { text: 'IP Replacer Settings' });
  
     new Setting(containerEl)
-      .setName('IP to Replace')
+      .setName('Placeholder')
       .setDesc('The IP or placeholder to be replaced.')
       .addText(text => text
         .setPlaceholder('Enter a string or regexp')
-        .setValue(this.plugin.toReplace)
+        .setValue(this.plugin.placeholder)
         .onChange(async (value) => {
-          this.plugin.setToReplace(value);
+          this.plugin.setPlaceholder(value);
         }));
 
     new Setting(containerEl)
-      .setName('IP Address')
-      .setDesc('The IP to replace your placeholder with')
+      .setName('Replacement')
+      .setDesc('Will replace the placeholder. Can be an interface (tun0) or an ip.')
       .addText(text => text
         .setPlaceholder('Enter IP address')
-        .setValue(this.plugin.ipAddress)
+        .setValue(this.plugin.replacement)
         .onChange(async (value) => {
-          this.plugin.setIPAddress(value);
+          this.plugin.setReplacement(value);
         }));
+
+    new Setting(containerEl)
+      .setName('Refresh Interfaces')
+      .setDesc('Reload the available network interfaces. Should be used when the interface did not yet exist when obsidian started.')
+      .addButton(button => {
+        button.setButtonText('Refresh')
+          .setCta()
+          .onClick(() => {
+            this.plugin.refreshInterfaces();
+          });
+      });
   }
 }
 
